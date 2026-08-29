c0e49291c3f4a018426955960e33aecad8854d33
jetaime520 <jetaime1201@gmail.com>
2026-08-30 01:15:56 +0800

    Add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './page';
+import './styles.css';
+
+createRoot(document.getElementById('root')!).render(<StrictMode><Home /></StrictMode>);
